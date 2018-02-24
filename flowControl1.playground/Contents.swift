8754e92be973d211567880a6cd8c917df563af51
West Kraemer <kraemerwest@gmail.com>
2018-02-24 10:52:29 -0500

    more problems

--- flowControl1.playground/Contents.swift
+++ flowControl1.playground/Contents.swift
@@ -1,5 +1,32 @@
-//: Playground - noun: a place where people can play
+//while loops
 
-import UIKit
+//while condition is true run the code below {
+//    loop code
+//}
 
-var str = "Hello, playground"
+var i = 10
+while i < 10 {
+    print(i)
+     i += 1
+}
+
+print("--")
+
+// this will always print the code at least the first time
+//repeat {
+//    loop code
+//} while condition {
+//
+//}
+
+i = 1
+repeat {
+    print(i)
+    if i == 5 {
+        break
+    }
+    i += 1
+} while i < 10
+
+print("-------")
+
